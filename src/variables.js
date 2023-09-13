a109c04b4a6566c036a53f0ad2a39408e7030f92
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-09-13 14:27:17 +0530

    initial comit

---
+++ src/variables.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var num = 10;
+// num = true cannot assign like this.cause num is number type
+num = 15;
+console.log(num);
+//can declare variables without assigning values
+var x;
+x = 15;
+console.log(x);
+// let y: String = "Ishara";
+// y = 10;
+// console.log(y);
+//above will print as js. but gives compiler error in watch
+/*reason : TypeScript helps catch
+type errors during development, but once your code is transpiled to JavaScript and executed,
+JavaScript's dynamic typing rules apply, and you won't receive runtime type errors for s
+uch assignments.*/
+var y = "Ishara";
+y = "chill";
+console.log(y);
