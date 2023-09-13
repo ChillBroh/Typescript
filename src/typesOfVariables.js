eba05ac7995a9bc254b4cd6e62ccc7e96c998bee
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-09-13 14:47:23 +0530

    basic datatypes done

---
+++ src/typesOfVariables.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//Basic data types
+//Number
+var x = 10;
+console.log(x);
+console.log(typeof x);
+//string
+var name = "TypeScript";
+//boolean
+var value = true;
+//undefined
+var u = undefined;
+var y;
+console.log(y);
+//null
+var n = null;
+var r = null;
+console.log(r);
+//other datatypes
---
+++ src/typesOfVariables.ts
@@ -0,0 +1,25 @@
+export {};
+
+//Basic data types
+//Number
+let x: number = 10;
+console.log(x);
+console.log(typeof x);
+
+//string
+let name: string = "TypeScript";
+
+//boolean
+let value: boolean = true;
+
+//undefined
+let u: undefined = undefined;
+let y;
+console.log(y);
+
+//null
+let n: null = null;
+let r = null;
+console.log(r);
+
+//other datatypes
